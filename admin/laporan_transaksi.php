850806024884249bd0cd30396bb2397d8cf7ebbd
BhanuW2067 <bhanuwijaksono@gmail.com>
2024-12-08 16:03:53 +0700

    Update laporan_transaksi.php

--- admin/laporan_transaksi.php
+++ admin/laporan_transaksi.php
@@ -195,8 +195,9 @@ if (isset($_POST['delete'])) {
                 <td>Rp<?= number_format($transaksi['jumlah'], 0, ',', '.') ?></td>
                 <td><?= htmlspecialchars($nasabah_tujuan['nama'] ?? 'N/A') ?></td>
                 <td>
-                    <form method="POST" style="display:inline;">
+                    <form method="POST" action="hapus_transaksi.php" style="display:inline;" onsubmit="return confirmDelete();">
                         <input type="hidden" name="transaksi_id" value="<?= $transaksi['id_transaksi'] ?>">
+                        <input type="hidden" name="id_nasabah" value="<?= $id_nasabah ?>"> <!-- Menyimpan id_nasabah untuk redirect -->
                         <button type="submit" name="delete" class="btn btn-danger btn-sm">Hapus</button>
                     </form>
                 </td>
@@ -212,6 +213,10 @@ function printReport(id) {
     printWindow.onload = function() {
         printWindow.print();
     };
+}
+
+function confirmDelete() {
+    return confirm("Apakah Anda yakin ingin menghapus transaksi ini?");
 }
 </script>
 <script src="../assets/js/bootstrap.bundle.min.js"></script>
